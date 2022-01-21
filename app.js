942a384400da53efb07b6b12ebc897f4ac667bfc
Reza <reza@atto.id>
2022-01-21 22:23:12 +0700

    env

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const app = require('express')();
 const PORT = process.env.PORT || 3000;
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 app.listen(PORT)

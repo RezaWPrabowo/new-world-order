70480324d74e49d4a8d5da1b6c76cffb08c717bb
Reza <reza@atto.id>
2022-01-21 22:28:08 +0700

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const app = require('express')();
-const PORT = process.env.PORT || 3000;
+const express = require('express')
+const app = express();
 const path = require('path');
+
+const PORT = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
